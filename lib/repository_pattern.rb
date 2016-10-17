ba4c167c5e9f6e703cee6a0aba8fa14bd2d07fbf
Joakim Reinert <mail@jreinert.com>
2016-10-17 15:22:05 +0200

    added missing documentation

--- lib/repository_pattern.rb
+++ lib/repository_pattern.rb
@@ -3,10 +3,17 @@ require 'repository_pattern/version'
 
 # The top level namespace
 module RepositoryPattern
+  # An error signaling an error with the configuration of the gem
   class ConfigurationError < RuntimeError; end
 
   module_function
 
+  # @overload models_namespace
+  #   Get the namespace where models are defined
+  # @overload models_namespace(namespace)
+  #   Set the namespace where models are defined
+  #   @param namespace [Module] The namespace module to be set
+  # @return [Module] the models namespace
   def models_namespace(namespace = nil)
     unless namespace || @models_namespace
       raise ConfigurationError, 'models namespace is unset'
